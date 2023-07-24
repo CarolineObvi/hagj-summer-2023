9209ec0bbb009a177e33e45a5848606c5fbfee2f
Tangerine61 <weyatwins@gmail.com>
2023-07-24 15:34:41 -0400

    graybox fbx commit

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -15,6 +15,9 @@ public class Move : MonoBehaviour
     private Vector2 targetVelocity;
     private Rigidbody2D body;
     private GroundCheck groundCheck;
+    //Noise Variable (Idle = 0, Crouch = 1, Walk = 2, Run = 3)
+    public int noiseProduced;
+    public bool isCrouching;
 
     private float maxSpeedChange;
     private float acceleration;
@@ -25,6 +28,8 @@ public class Move : MonoBehaviour
     {
         body = GetComponent<Rigidbody2D>();
         groundCheck = GetComponent<GroundCheck>();
+        //crouch check
+        isCrouching = false;
     }
 
     // Update is called once per frame
@@ -32,6 +37,17 @@ public class Move : MonoBehaviour
     {
         direction.x = input.RetrieveMoveInput();
         targetVelocity = new Vector2(direction.x, 0f) * Mathf.Max(maxSpeed - groundCheck.GetFriction(), 0f);
+
+        //Noise
+
+         if (Input.GetKeyDown(KeyCode.C))
+            isCrouching = true;
+
+         if (Input.GetKeyUp(KeyCode.C))
+            isCrouching = false;
+
+        if (isCrouching == true)
+            noiseProduced = 1;
     }
 
     void FixedUpdate()
@@ -45,4 +61,6 @@ public class Move : MonoBehaviour
 
         body.velocity = velocity;
     }
+
+
 }
